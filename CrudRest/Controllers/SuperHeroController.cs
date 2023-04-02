abf49ceb889e3bfd786c4108bbe419715fa53258
chamodya wimansha <chamodyawimansha@gmail.com>
2023-04-02 10:52:24 +0530

    put method

--- CrudRest/Controllers/SuperHeroController.cs
+++ CrudRest/Controllers/SuperHeroController.cs
@@ -33,11 +33,41 @@ namespace CrudRest.Controllers
             return Ok(this.heroes);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+
+            if (hero == null)
+            {
+                return BadRequest("Hero not found");
+            }
+
+            return Ok(hero);
+        }
+
         [HttpPost]
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
             heroes.Add(hero);
             return Ok(this.heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<SuperHero>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+            {
+                return BadRequest("Hero not found");
+            }
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            return Ok(hero);
         }
     }
 }
